f5fc1bdaf428e31d6829405e1135a278cc91890d
gguidoxx <gudombe@gmail.com>
2022-10-27 23:04:50 -0300

    Empiezo estilos card, manejando responsive

--- src/App.js
+++ src/App.js
@@ -2,5 +2,9 @@ import "./App.css";
 import Aboutme from "./components/AboutMe/aboutme";
 
 export default function App() {
-  return <Aboutme />;
+  return (
+    <>
+      <Aboutme />
+    </>
+  );
 }
